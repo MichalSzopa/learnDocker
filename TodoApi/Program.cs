ca05103447604d869439c6aae4f164a2d5f8b7b8
Michał Szopa <32485285+MichalSzopa@users.noreply.github.com>
2023-11-16 15:00:28 +0100

    initial commit

---
+++ TodoApi/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Cors;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("Allow8080",
+        builder => 
+        {
+            builder.WithOrigins("http://localhost:8080")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("Allow8080");
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
